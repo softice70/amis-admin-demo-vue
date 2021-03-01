f5b82665f8ec397f6d12545a8870d536031bce54
softice70 <dawnsun@sina.com>
2021-03-01 15:54:24 +0800

    vue3.0框架下使用amis的例程

---
+++ src/components/amis-demo/amis-demo.vue
@@ -0,0 +1,31 @@
+<template>
+    <AMisRenderer :schema="schema" />
+</template>
+
+<script>
+import AMisRenderer from "@/components/amis/AMisRenderer.vue";
+
+export default {
+    name: "AmisDemo",
+    components: {
+        AMisRenderer
+    },
+    data() {
+        return {
+            schema: {
+                type: 'app',
+                brandName: 'Admin',
+                logo: '/logo.png',
+                header: {
+                    type: 'tpl',
+                    inline: false,
+                    className: 'w-full',
+                    tpl:
+                        '<div class="flex justify-between"><div>顶部区域左侧</div><div>顶部区域右侧</div></div>'
+                },
+                api: '/pages/site.json'
+            }
+        };
+    }
+};
+</script>
